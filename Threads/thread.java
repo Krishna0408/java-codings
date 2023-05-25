798036ffee33c4bc841ac7010fed0303579db8cb
Krishna Prasath <98581299+Krishna0408@users.noreply.github.com>
2023-05-25 14:46:58 +0530

    Add files via upload

---
+++ Threads/thread.java
@@ -0,0 +1,19 @@
+class thread
+{
+    public static void main(String [] args) {
+    try 
+{ 
+    int x = 0; 
+    int y = 5 / x; 
+} 
+catch (ArithmeticException ae) 
+{
+    System.out.println(" Arithmetic Exception"); 
+} 
+catch (Exception e) 
+{
+    System.out.println("Exception"); 
+} 
+System.out.println("finished");
+}
+}
